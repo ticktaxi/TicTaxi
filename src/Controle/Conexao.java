053951fd6d78d67980dea12f2d4944d18406938a
Guilherme Donda <guilherme_hda@hotmail.com>
2014-06-19 16:47:54 -0300

    Teste de pc

--- src/Controle/Conexao.java
+++ src/Controle/Conexao.java
@@ -14,7 +14,7 @@ public class Conexao {
            return c;
        }
        catch(Exception e){
-           JOptionPane.showMessageDialog(null, "Erro ao conectar com o Banco  ");
+           JOptionPane.showMessageDialog(null, "Erro ao conectar com o Banco");
            e.printStackTrace();
            return null;
        }
